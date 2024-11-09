834660cbfd14d40817ac7af35fa76ab1393f752e
Julie <julie23@gmail.com>
2024-11-08 20:07:01 -0600

    pathway3lesson1

--- script3.js
+++ script3.js
@@ -12,9 +12,17 @@ function serverpurpose(req, res) {
         let number1 = parameter.a
         let number2 = parameter.b
         let multiply = parseInt(number1)* parseInt(number2)
-        res.write(multiply)
-
-
+        res.write(`<p>the result is ${multiply}</p>`)
+    }else if(urldata.pathname === "/divide"){
+        let num1 = parameter.a
+        let num2 = parameter.b
+        let divide = parseInt(num1) / parseInt(num2)
+        res.write(`<p>the result is ${divide}</p>`)
+    }else if(urldata.pathname === "/subtract"){
+        let numb1 = parameter.a
+        let numb2 = parameter.b
+        let subtract = parseInt(numb1)- parseInt(numb2)
+        res.write(`<p>the result is ${subtract}</p>`)
     }
 
     res.end()
